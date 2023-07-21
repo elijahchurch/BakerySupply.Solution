c48787fdbc26c73de1206271819c15870cd1cbba
Jason E. Church <elijahchurch@gmail.com>
2023-07-21 12:02:52 -0700

    Create New.cshtml file with form to add new Vendor. Create Post route that redirects back to Vendor list page

--- BakerySupply/Controllers/VendorController.cs
+++ BakerySupply/Controllers/VendorController.cs
@@ -13,5 +13,17 @@ namespace BakerySupply.Controllers
             List<Vendor> allVendors = Vendor.GetAll();
             return View(allVendors);
         }
+        [HttpGet("/vendors/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost("/vendors")]
+        public ActionResult Create(string name, string description)
+        {
+            Vendor newVendor = new Vendor(name, description);
+            return RedirectToAction("Index");
+        }
     }
 }
